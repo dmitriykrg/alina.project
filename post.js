de1a1be80a9799b44d271e73ecf1c4ecac398cff
dmitriykrg <mrlokichannel@gmail.com>
2020-11-01 23:18:14 +0300

    Пытаюсь починить выбор

--- post.js
+++ post.js
@@ -1,12 +1,17 @@
 let wrap= document.querySelector(".wrapper")
 $.getJSON( "post.json", function( data ) {
     
-    data.map(item=>{
+    data.map((item,index)=>{
         
-        wrap.innerHTML+='<div class="post">'+'<img width="100%"  src="'+item.src+' "> '+item.name+'</div> </br>';
-       
+        wrap.innerHTML+='<div class="post">'+'<img width="100%"  src="'+item.src+' "> '+' <div class="target'+ index +'"> </div> </div > </br>';
+      
         
     })
-
+    wrap.addEventListener('mouseover',function (event){
+        let target = event.target;
+        if(target.className !== 'wrapper'){
+        
+        }
+    })
 
 });

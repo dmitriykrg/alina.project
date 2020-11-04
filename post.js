a660d70c6f753bc5d2607bdcfb2f8edfd1424870
dmitriykrg <mrlokichannel@gmail.com>
2020-11-04 16:59:43 +0300

    PretSet

--- post.js
+++ post.js
@@ -1,17 +1,26 @@
-let wrap= document.querySelector(".wrapper")
+
+$(function(){
+let wrap= document.querySelector(".wrapper");
 $.getJSON( "post.json", function( data ) {
     
     data.map((item,index)=>{
         
-        wrap.innerHTML+='<div class="post">'+'<img width="100%"  src="'+item.src+' "> '+' <div class="target'+ index +'"> </div> </div > </br>';
+        wrap.innerHTML+='<img width=20% class="post" src="'+item.src+' "> '+'</br>';
       
         
     })
     wrap.addEventListener('mouseover',function (event){
         let target = event.target;
         if(target.className !== 'wrapper'){
-        
+            console.log('in')
         }
     })
+    wrap.addEventListener('mouseout',function (event){
+        let target = event.target;
+       
+            console.log('out')
+        
+    })
 
+});
 });

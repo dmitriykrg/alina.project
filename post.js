46b31b5d8ab1d00f0b9b7de181a6771227698ce7
dmitriykrg <mrlokichannel@gmail.com>
2020-11-04 20:26:44 +0300

    Finnaly pochti

--- post.js
+++ post.js
@@ -5,24 +5,10 @@ $.getJSON( "post.json", function( data ) {
     
     data.map((item,index)=>{
         
-        wrap.innerHTML+='<div class=post id = "post" data-name ='+item.name+'><img width=100% src = '+item.src+'></div>'+'</br>';
-      
         
-    })
-    wrap.addEventListener('mouseover',function (event){
-        let target = event.target;
-        if(target.className !== 'wrapper'){
-            let text = document.getElementById(target.id);
-            
-            
-        }
-    })
-    wrap.addEventListener('mouseout',function (event){
-        let target = event.target;
-        let text = document.getElementById(target.id);
-      
-        
-    })
+        wrap.innerHTML+='<div class=col><div class=post id = "post" data-name ='+item.name+'><img width=100% src = '+item.src+'></div>'+'</br>';
 
+
+    })  
 });
 });

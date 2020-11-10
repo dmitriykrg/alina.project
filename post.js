1014dcec9fe393bb8f25ef58e2c3ae4d1f200d77
dmitriykrg <mrlokichannel@gmail.com>
2020-11-10 07:24:43 +0300

    'front'

--- post.js
+++ post.js
@@ -1,20 +1,44 @@
 
 
-
 let wrap= document.querySelector(".wrapper");
 
-        function lala (){};
-        () =>{}
-        check => {}
 
+       
  fetch("post.json").then(response => response.json()).then(json=>   json.map((item)=>{
         
-        
-    wrap.innerHTML+='<div class=col><div class=post id = "post" data-name ='+item.name+'><img width=100% src = '+item.src+'></div>'+'</br>';
+let ust ='';
+let ustCL='';
 
+        if(item.pos==1){
+          ust = 'установлено'
+          ustCL = 'btnU'
+        } else {
+          ust = 'не установлено';
+          ustCL = 'btnUN'
+      }
+    wrap.innerHTML+=`<div class=col><div class=post><label class="lab" id=${item.id} datatag="checked"><img width=100% src = ${item.src}></label><span class="name">${item.name}</span><button class=${ustCL}>${ust}</button</div></div>
+    `
+    
+    
 
-})  
+    
+ })
 );
+
+
+$('.wrapper').delegate('label','click',function(data){
+    
+    let id = data.currentTarget.id;
+    
+    let check = data.currentTarget.attributes.datatag.value
+  
+Swal.fire({
+    title: `Этот виджет имеет id=${id} и следовательно может ${check} получать прочие данные`,
+    width: '60rem'
+  })
+
+})
+
 
 
 
